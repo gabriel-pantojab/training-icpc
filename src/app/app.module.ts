ce29194e06e7057e0ebd4291c861ea6baaf4a288
GabrielPB96 <gabrieljuniorpb990@gmail.com>
2023-09-18 06:50:11 -0300

    [ADD] tag component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { TagComponent } from './components/tag/tag.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TagComponent],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
