3eeba7047734d9ccb23a13a784379021b15d1ef9
GabrielPB96 <gabrieljuniorpb990@gmail.com>
2023-09-18 06:43:00 -0300

    [ADD] filter form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { CodeforcesService } from './services/codeforces.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'random-problem';
+  filterForm: FormGroup;
+  constructor(private codeforcesService: CodeforcesService) {
+    this.filterForm = new FormGroup({
+      minDifficulty: new FormControl(800),
+      maxDifficulty: new FormControl(3600),
+      tag: new FormControl(''),
+    });
+  }
+
+  onSubmit(event: Event) {}
 }
