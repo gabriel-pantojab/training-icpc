13baad3e6cc4e2e8bf57c5442be4b36778df29d1
GabrielPB96 <gabrieljuniorpb990@gmail.com>
2023-09-17 23:16:21 -0300

    [ADD] codeforces service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CodeforcesService } from './services/codeforces.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'random-problem';
+
+  constructor(private codeforcesService: CodeforcesService) {}
+
+  ngOnInit(): void {
+    this.codeforcesService
+      .getProblemsByTagsAndDifficulty({
+        tags: ['implementation'],
+        minDifficulty: 1000,
+        maxDifficulty: 1500,
+      })
+      .then((problems) => {
+        console.log(problems);
+      });
+  }
 }
---
+++ src/app/services/codeforces.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+
+interface Problem {
+  contestId: number;
+  problemsetName: string;
+  index: string;
+  name: string;
+  type: ProblemType;
+  points: number;
+  rating?: number;
+  tags: string[];
+}
+
+enum ProblemType {
+  PROGRAMMING = 'PROGRAMMING',
+  QUESTION = 'QUESTION',
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CodeforcesService {
+  private readonly API_URL = 'https://codeforces.com/api';
+
+  constructor() {}
+
+  async getProblemsByTagsAndDifficulty({
+    tags,
+    minDifficulty,
+    maxDifficulty,
+  }: {
+    tags: string[];
+    minDifficulty: number;
+    maxDifficulty: number;
+  }): Promise<Problem[]> {
+    const response = await fetch(
+      `${this.API_URL}/problemset.problems?tags=${tags.join(';')}`
+    );
+    const data = await response.json();
+    if (data.status !== 'OK') throw new Error('Codeforces API error');
+    const problems: Problem[] = data.result.problems.filter(
+      (problem: Problem) =>
+        problem.rating &&
+        problem.rating >= minDifficulty &&
+        problem.rating <= maxDifficulty
+    );
+    return problems;
+  }
+}
