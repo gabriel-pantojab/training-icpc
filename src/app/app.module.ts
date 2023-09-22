54e720d1d8c2e5f3248b9975372b0eda9595ad4f
GabrielPB96 <gabrieljuniorpb990@gmail.com>
2023-09-22 00:18:07 -0300

    [ADD] loading component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { TagComponent } from './components/tag/tag.component';
 import { ProblemComponent } from './components/problem/problem.component';
 import { FilterCodeforcesComponent } from './components/filter-codeforces/filter-codeforces.component';
+import { LoadingComponent } from './components/loading/loading.component';
 
 @NgModule({
-  declarations: [AppComponent, TagComponent, ProblemComponent, FilterCodeforcesComponent],
+  declarations: [AppComponent, TagComponent, ProblemComponent, FilterCodeforcesComponent, LoadingComponent],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
