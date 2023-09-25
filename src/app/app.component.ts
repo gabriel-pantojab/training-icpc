665a93468e269a89ed88ceb5de694ce7b689bb65
GabrielPB96 <gabrieljuniorpb990@gmail.com>
2023-09-25 20:25:38 -0300

    [ADD] pagination

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
 import { Problem } from './services/codeforces.service';
 
 @Component({
@@ -9,10 +9,34 @@ import { Problem } from './services/codeforces.service';
 export class AppComponent {
   title = 'Training ICPC';
   problems = signal<Problem[] | null>([]);
+  renderProblems = computed<Problem[] | null>(() => {
+    const problems = this.problems();
+    if (!problems) return null;
+    const page = this.page();
+    const start = (page - 1) * 100;
+    const end = start + 100;
+    return problems.slice(start, end);
+  });
+  countPages = signal<number>(0);
+  page = signal<number>(1);
 
   constructor() {}
 
   setProblems(problems: Problem[] | null) {
     this.problems.set(problems);
+    if (problems) {
+      this.countPages.set(Math.ceil(problems.length / 100));
+      this.page.set(1);
+    }
+  }
+
+  nextPage() {
+    if (this.page() + 1 > this.countPages()) return;
+    this.page.set(this.page() + 1);
+  }
+
+  prevPage() {
+    if (this.page() - 1 < 1) return;
+    this.page.set(this.page() - 1);
   }
 }
