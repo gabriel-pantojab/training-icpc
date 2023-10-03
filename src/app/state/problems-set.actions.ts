88fe355d5c7ca23a8e44a41a0d7ce998c4868f01
GabrielPB96 <gabrieljuniorpb990@gmail.com>
2023-10-02 22:55:05 -0300

    [ADD] problems set actions

---
+++ src/app/state/problems-set.actions.ts
@@ -0,0 +1,14 @@
+import { createAction, props } from '@ngrx/store';
+import { ProblemAPI } from '../models/model';
+
+export const init = createAction('[Main Page] Init');
+
+export const updateProblems = createAction(
+  '[Main Page] Update Problems',
+  props<{ problems: ProblemAPI[] }>()
+);
+
+export const updateProblemsRender = createAction(
+  '[Main Page] Update Problems Render',
+  props<{ problemsRender: ProblemAPI[] }>()
+);
