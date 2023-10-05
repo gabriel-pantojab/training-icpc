dfbf80e2e6f7e2b1338a91c119995df4aaa51fbf
GabrielPB96 <gabrieljuniorpb990@gmail.com>
2023-10-05 00:48:27 -0300

    [UPDATE] problem set service

--- src/app/components/problem-set/problem-set.component.ts
+++ src/app/components/problem-set/problem-set.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, computed, signal } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProblemAPI } from 'src/app/models/model';
-import { CodeforcesService } from 'src/app/services/codeforces.service';
+import { ProblemSetService } from 'src/app/services/problem-set/problem-set.service';
 
 @Component({
   selector: 'app-problem-set',
@@ -9,50 +9,22 @@ import { CodeforcesService } from 'src/app/services/codeforces.service';
 })
 export class ProblemSetComponent implements OnInit {
   title = 'Problem Set';
-  problems = signal<ProblemAPI[] | null>([]);
-  renderProblems = computed<ProblemAPI[] | null>(() => {
-    const problems = this.problems();
-    if (!problems) return null;
-    const page = this.page();
-    const start = (page - 1) * 100;
-    const end = start + 100;
-    return problems.slice(start, end);
-  });
-  countPages = signal<number>(0);
-  page = signal<number>(1);
-
-  constructor(private codeforcesService: CodeforcesService) {}
+  constructor(public problemSetService: ProblemSetService) {}
 
   ngOnInit() {
-    this.loadDefaultProblems();
-  }
-
-  async loadDefaultProblems() {
-    this.setProblems(null);
-    const problems =
-      await this.codeforcesService.getProblemsByTagsAndDifficulty({
-        tags: [],
-        minDifficulty: 800,
-        maxDifficulty: 3500,
-      });
-    this.setProblems(problems);
+    if (this.problemSetService.renderProblems()?.length === 0)
+      this.problemSetService.loadDefaultProblems();
   }
 
   setProblems(problems: ProblemAPI[] | null) {
-    this.problems.set(problems);
-    if (problems) {
-      this.countPages.set(Math.ceil(problems.length / 100));
-      this.page.set(1);
-    }
+    this.problemSetService.setProblems(problems);
   }
 
   nextPage() {
-    if (this.page() + 1 > this.countPages()) return;
-    this.page.set(this.page() + 1);
+    this.problemSetService.nextPage();
   }
 
   prevPage() {
-    if (this.page() - 1 < 1) return;
-    this.page.set(this.page() - 1);
+    this.problemSetService.prevPage();
   }
 }
--- src/app/services/problem-set/problem-set.service.ts
+++ src/app/services/problem-set/problem-set.service.ts
@@ -1,9 +1,53 @@
-import { Injectable } from '@angular/core';
+import { Injectable, computed, signal } from '@angular/core';
+import { ProblemAPI } from 'src/app/models/model';
+import { CodeforcesService } from '../codeforces.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProblemSetService {
+  problems = signal<ProblemAPI[] | null>([]);
+  renderProblems = computed<ProblemAPI[] | null>(() => {
+    const problems = this.problems();
+    if (!problems) return null;
+    const page = this.page();
+    const start = (page - 1) * 100;
+    const end = start + 100;
+    return problems.slice(start, end);
+  });
+  countPages = signal<number>(0);
+  page = signal<number>(1);
 
-  constructor() { }
+  constructor(private codeforcesService: CodeforcesService) {}
+  hola() {
+    console.log('hola');
+  }
+  async loadDefaultProblems() {
+    this.setProblems(null);
+    const problems =
+      await this.codeforcesService.getProblemsByTagsAndDifficulty({
+        tags: [],
+        minDifficulty: 800,
+        maxDifficulty: 3500,
+      });
+    this.setProblems(problems);
+  }
+
+  setProblems(problems: ProblemAPI[] | null) {
+    this.problems.set(problems);
+    if (problems) {
+      this.countPages.set(Math.ceil(problems.length / 100));
+      this.page.set(1);
+    }
+  }
+
+  nextPage() {
+    if (this.page() + 1 > this.countPages()) return;
+    this.page.set(this.page() + 1);
+  }
+
+  prevPage() {
+    if (this.page() - 1 < 1) return;
+    this.page.set(this.page() - 1);
+  }
 }
