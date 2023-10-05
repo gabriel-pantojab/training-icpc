e52c08283754cde5f2124ba045dca3088f09b5e8
GabrielPB96 <gabrieljuniorpb990@gmail.com>
2023-10-05 00:09:13 -0300

    [ADD] todo problem

--- src/app/components/my-problems/my-problems.component.ts
+++ src/app/components/my-problems/my-problems.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Problem } from 'src/app/models/model';
+import { TodosSelectors } from 'src/app/state';
 
 @Component({
   selector: 'app-my-problems',
@@ -7,4 +11,9 @@ import { Component } from '@angular/core';
 })
 export class MyProblemsComponent {
   title = 'My Problems';
+  todoProblem$: Observable<Problem[]> = this.store.select(
+    TodosSelectors.problems
+  );
+
+  constructor(private store: Store) {}
 }
