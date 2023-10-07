60909f3f34f455193fdb293e454e9506710c5f9c
GabrielPB96 <gabrieljuniorpb990@gmail.com>
2023-10-07 14:41:21 -0300

    [ADD] angular fire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,11 @@ import { todosReducer } from './state';
 import { TodoProblemComponent } from './components/todo-problem/todo-problem.component';
 import { ProblemListDateComponent } from './components/problem-list-date/problem-list-date.component';
 import { ObjToArrayPipe } from './pipes/obj-to-array.pipe';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 const routes: Routes = [
   {
@@ -53,6 +58,10 @@ const routes: Routes = [
     StoreModule.forRoot({
       todosState: todosReducer,
     }),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideStorage(() => getStorage()),
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'training-icpc',
+    appId: '1:332791973283:web:821c40fcf6598c7878c616',
+    storageBucket: 'training-icpc.appspot.com',
+    apiKey: 'AIzaSyCyDrhYWQgmym3_Pw3zZUuT6kUmWZW0e58',
+    authDomain: 'training-icpc.firebaseapp.com',
+    messagingSenderId: '332791973283',
+    measurementId: 'G-DZRRK3MP28',
+  },};
