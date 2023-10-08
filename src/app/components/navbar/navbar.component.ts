62c420d2d1d93aeeb03e2be7044ec3cfa9a99ab2
GabrielPB96 <gabrieljuniorpb990@gmail.com>
2023-10-08 19:25:25 -0300

    [ADD] user info

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
@@ -8,12 +8,25 @@ import { AuthService } from 'src/app/services/auth/auth.service';
 })
 export class NavbarComponent {
   authService = inject(AuthService);
-
+  visibleButtonAuth = signal<boolean>(false);
   handleAuth() {
     if (this.authService.user) {
       this.authService.signOut();
     } else {
       this.authService.signInGoogle();
     }
+  }
+
+  getClassesUserInfo() {
+    if (this.authService.isLogged() && !this.visibleButtonAuth()) {
+      return 'hidden';
+    }
+    if (this.authService.isLogged()) return 'visible';
+    return '';
+  }
+
+  toggleVisibleUserInfo() {
+    console.log(this.authService.user);
+    this.visibleButtonAuth.set(!this.visibleButtonAuth());
   }
 }
