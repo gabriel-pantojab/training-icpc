b20751d3b90eb2143cc169a4a54428d66a56b00d
Gabriel Bustamante <gabrieljuniorpb990@gmail.com>
2024-11-19 19:41:51 -0400

    [FIX]: firebase configuration

---
+++ src/app/app.config.ts
@@ -0,0 +1,35 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { provideStore } from '@ngrx/store';
+import { routes } from './app.routes';
+import { todosReducer } from './state';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'training-icpc',
+        appId: '1:332791973283:web:821c40fcf6598c7878c616',
+        storageBucket: 'training-icpc.appspot.com',
+        apiKey: 'AIzaSyCyDrhYWQgmym3_Pw3zZUuT6kUmWZW0e58',
+        authDomain: 'training-icpc.firebaseapp.com',
+        messagingSenderId: '332791973283',
+        measurementId: 'G-DZRRK3MP28',
+        databaseURL: 'https://training-icpc-default-rtdb.firebaseio.com/',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase()),
+    provideStore({
+      todoState: todosReducer,
+    }),
+  ],
+};
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,19 @@
 import { Component, inject, signal } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from 'src/app/services/auth/auth.service';
+import { UserCardComponent } from '../user-card/user-card.component';
 
 @Component({
   selector: 'app-navbar',
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive, UserCardComponent],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
   authService = inject(AuthService);
+
+  get isLogged() {
+    return this.authService.isLogged();
+  }
 }
