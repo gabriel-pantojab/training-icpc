6087cb709a02deb33e6143b3d7e4d9ffa0ceb680
Gabriel Bustamante <gabrieljuniorpb990@gmail.com>
2024-12-08 19:25:20 -0400

    refactor: remove unused components and associated files

--- src/app/components/loading/loading.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-loading',
-  standalone: true,
-  templateUrl: './loading.component.html',
-  styleUrls: ['./loading.component.css'],
-})
-export class LoadingComponent {}
